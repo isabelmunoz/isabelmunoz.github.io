b79f587843d4c5224d597affee7b5d4c98da827e
Isabel <ishamu023@gmail.com>
2020-03-29 18:17:04 -0300

    animación de menu

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,27 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+/* MENU FIJO EN PANTALLA*/
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        scroll=$(window).scrollTop();
+        if(scroll > 100){
+            $(".menu").css({"position":"fixed"});
+            $(".menu").css({"width":"100%"});
+            $(".menu").css({"top":"0"});
+            $(".menu").css({"background":"#fff"});
+            $(".menu a").css({"color":"#000"});
+            $(".logo").css({"color":"#000"});
+            $(".menu").css({"box-shadow":"rgba(70, 66, 66, 0.5) 6px 1px 8px"});
+            $(".menu").css({"z-index":"10"});
+        }else{
+            $(".menu").css({"position":"relative"});
+            $(".menu").css({"background":"transparent"});
+            $(".menu").css({"box-shadow":"0 0 0"});
+            $(".menu a").css({"color":"#fff"});
+            $(".logo").css({"color":"#fff"});
+        }
+    })
+})
